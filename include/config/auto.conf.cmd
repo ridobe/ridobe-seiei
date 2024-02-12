ce945823340ac0f7c3cc29b3ff24ad68d6032ecc
Dodd Berry <ridobe18@gmail.com>
2024-02-11 20:05:16 -0500

    Merge tag 'v6.8-rc3' into ridobe-seiei

--- include/config/auto.conf.cmd
+++ include/config/auto.conf.cmd
@@ -1571,7 +1571,7 @@ include/config/auto.conf: $(deps_config)
 ifneq "$(ARCH)" "x86"
 include/config/auto.conf: FORCE
 endif
-ifneq "$(KERNELVERSION)" "6.8.0-rc2"
+ifneq "$(KERNELVERSION)" "6.8.0-rc3"
 include/config/auto.conf: FORCE
 endif
 ifneq "$(CC)" "clang"
